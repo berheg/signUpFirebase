9ec74e5e5273894e92629ee0aae55936d088c703
Girmay <ink423103@yahoo.com>
2019-12-15 23:27:44 +0100

    Updates

---
+++ precache-manifest.50e4815c25204d52daa4b76dd5c62138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9989403b0bb5c122c8b1acd5643cc49d",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "9ad008fc2d8cf7853a12",
+    "url": "/my-app/static/css/main.10333e92.chunk.css"
+  },
+  {
+    "revision": "fead55e4ace152a84dfc",
+    "url": "/my-app/static/js/2.505fbf60.chunk.js"
+  },
+  {
+    "revision": "cfae6de4e4fba91c363679a64a6e3590",
+    "url": "/my-app/static/js/2.505fbf60.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ad008fc2d8cf7853a12",
+    "url": "/my-app/static/js/main.c3f2be61.chunk.js"
+  },
+  {
+    "revision": "90f9974f79f26637c5e1",
+    "url": "/my-app/static/js/runtime-main.93ea6051.js"
+  }
+]);
